a2b84568402f9113af862c7a278c824b333576a0
andrea longhi <andrea@spaghetticode.it>
2012-10-16 06:41:04 +0200

    todo model specs are green

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,8 @@
 class Todo < ActiveRecord::Base
   attr_accessible :name, :completed, :list_id, :list
+
+  belongs_to :list
+
+  validates_presence_of   :name
+  validates_uniqueness_of :name, :scope => :list_id
 end
